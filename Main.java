417e66d8471e5077c98041180c822822d313a596
V10Tur <72814308+V10Tur@users.noreply.github.com>
2020-10-26 18:56:45 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        int sum = 0;
+        int j = 1;
+        while(j++<=100)
+        {
+            sum += j;
+
+        }
+        System.out.print(sum);
+    }
+}
